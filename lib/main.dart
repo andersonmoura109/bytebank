54283d8d1441d1af6db458d863e8ecd0fe9e7650
Anderson Moura <andersonmoura109@gmail.com>
2022-07-02 02:20:12 -0300

    Created TransferItem class

--- lib/main.dart
+++ lib/main.dart
@@ -6,28 +6,7 @@ void main() => runApp(
           appBar: AppBar(
             title: const Text('Transfers'),
           ),
-          body: Column(
-            children: const [
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.monetization_on,
-                  ),
-                  title: Text('100.00'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(
-                    Icons.monetization_on,
-                  ),
-                  title: Text('200.00'),
-                  subtitle: Text('1000'),
-                ),
-              ),
-            ],
-          ),
+          body: const TransferList(),
           floatingActionButton: FloatingActionButton(
             onPressed: () {},
             child: const Icon(
@@ -37,3 +16,38 @@ void main() => runApp(
         ),
       ),
     );
+
+class TransferList extends StatelessWidget {
+  const TransferList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TransferItem(value: '100.00', account: '1000'),
+        TransferItem(value: '200.00', account: '1001'),
+        TransferItem(value: '300.00', account: '1002'),
+      ],
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final String value;
+  final String account;
+
+  TransferItem({required this.value, required this.account});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(
+          Icons.monetization_on,
+        ),
+        title: Text(value),
+        subtitle: Text(account),
+      ),
+    );
+  }
+}
